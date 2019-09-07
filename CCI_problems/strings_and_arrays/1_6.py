9520a2210f6bdf7e65c162122c19f647613056a8
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-07 15:21:57 -0500

    1_6

---
+++ CCI_problems/strings_and_arrays/1_6.py
@@ -0,0 +1,33 @@
+#Strting compression
+
+#Time complexity O(n)
+
+def string_compression(string):
+
+    if len(string) == 0:
+        return 0
+
+    if len(string) == 1:
+        return string + "1"
+
+    count = 1
+    curr_char = string[0]
+    new_str = ""
+
+    for pos in range(1,len(string)):
+        if curr_char == string[pos]:
+            count += 1
+
+            if pos == len(string) - 1:
+                new_str += "%s%d" % (curr_char, count)
+
+        else:
+            new_str += "%s%d" % (curr_char, count)
+            count = 1
+            curr_char = string[pos]
+
+
+    if len(new_str) < len(string):
+        return new_str
+
+    return string

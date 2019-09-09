3ad0f91b42a0fbfa79552a3f151695335e068f39
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-08 23:39:27 -0500

    1_8 complete

--- CCI_problems/strings_and_arrays/1_8.py
+++ CCI_problems/strings_and_arrays/1_8.py
@@ -1,22 +1,36 @@
 #Zero matrix
 
-#Time complexity O(n^3)
+#Time complexity O(n^2)
 
 def fill_zeros(matrix, row, col):
-    for i in range(len(matrix)):
-        matrix[i][row] = 0
+    for pos_i in range(len(matrix)):
+        matrix[pos_i][col] = 0
 
-    for j in range(len(matrix[0])):
-        matrix[col][j] = 0
+    for pos_j in range(len(matrix[0])):
+        matrix[row][pos_j] = 0
 
     return matrix
 
 
-def zero_matrix_solution(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == 0:
-                matrix = fill_zeros(matrix, i, j)
-                return matrix
+def find_zeros(matrix):
+    arr_pos = []
+
+    for pos_i in range(len(matrix)):
+        for pos_j in range(len(matrix[0])):
+            if matrix[pos_i][pos_j] == 0:
+                arr_pos.append((pos_i,pos_j))
+
+
+    return arr_pos
+
+
+def solution(matrix):
+    if len(matrix) == 0:
+        return 0
+
+    arr_pos = find_zeros(matrix)
+
+    for value in arr_pos:
+        matrix = fill_zeros(matrix, value[0], value[1])
 
     return matrix

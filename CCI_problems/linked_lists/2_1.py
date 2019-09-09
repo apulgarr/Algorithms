35bc2775c0792fdde851e2da51a95f8fb9e8d850
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-09 16:47:22 -0500

    2_1 two different solutions

--- CCI_problems/linked_lists/2_1.py
+++ CCI_problems/linked_lists/2_1.py
@@ -1,1 +1,44 @@
-##
+#Delete duplicates: Delete the elements duplicate in a linked list
+
+#Time complexity: O(n^2)
+#Brute force solution with pointers
+
+def delete_duplicates(node):
+    if not node:
+        return 0
+
+    while node:
+        visitor_p = node
+        visitor = node.next
+
+        while visitor:
+            if node.value == visitor.value:
+                    visitor_p.next = visitor.next
+                    visitor = visitor_p.next
+
+            else:
+                visitor_p = visitor
+                visitor = visitor.next
+
+    return node
+
+
+#Time complexity: O(n)
+#Optimized
+
+def delete_duplicates_optimized(node):
+    no_duplicates = {}
+    curr_node = node
+    p_node = None
+
+    while curr_node:
+        if curr_node.value not in no_duplicates.keys():
+            no_duplicates[curr_node.value] = True
+            p_node = curr_node
+            curr_node = curr_node.next
+
+        else:
+            p_node.next = curr_node.next
+            curr_node = p_node.next
+
+    return node

652c02432a4fb3000aa3dcb9f9bdb8e2d8d710ed
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-11 21:02:14 -0500

    cleaning the code

--- CCI_problems/linked_lists/2_5.py
+++ CCI_problems/linked_lists/2_5.py
@@ -1,29 +1,34 @@
+#Given two linked lists, sum their values and return the result node
+
+#Time complexity: O(max(m,n))
+
+
 class Node(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        sum = 0
-        l_aux = None
-        
-        while l1 or l2 or sum:
-            if l1:
-                sum += l1.val
-                l1 = l1.next
-            
-            if l2:
-                sum += l2.val
-                l2 = l2.next 
-                
-            if l_aux:
-                l_aux.next = Node(sum%10)
-                l_aux = l_aux.next
-            else:
-                l3 = Node(sum%10)
-                l_aux = l3
-                
-            sum = sum / 10
-        
-        return l3
+
+def add_two_numbers(l1, l2):
+    sum = 0
+    l_aux = None
+
+    while l1 or l2 or sum:
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+
+        if l_aux:
+            l_aux.next = Node(sum%10)
+            l_aux = l_aux.next
+        else:
+            l3 = Node(sum%10)
+            l_aux = l3
+
+        sum = sum / 10
+
+    return l3

50e5ce8e9eeec84c6fb63ff3c9cf0448a5bc1160
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-11 19:51:36 -0500

    2_3 complete

--- CCI_problems/linked_lists/2_3.py
+++ CCI_problems/linked_lists/2_3.py
@@ -1,10 +1,12 @@
+from linked_list import Linked_list
+
 #Delete middle node into the list
 
 #Time complexity: O(n)
 
 def get_len(node):
     if not node:
-        return None
+        return 0
 
     return 1 + get_len(node.next)
 
@@ -13,12 +15,14 @@ def check_middle(node, target):
     return node.value == target
 
 
-def delete_middle(node, targe):
-    count = get_len(node) / 2
-    middle = count
+def delete_middle(node, target):
+    head_node = node
+    middle = get_len(node)
+    count = middle/2
+    p_node = None
 
-    while count - 1 > 0:
-        if middle == 1:
+    while count > 0:
+        if count == 1:
             aux_node = p_node
 
         p_node = node
@@ -27,13 +31,21 @@ def delete_middle(node, targe):
 
     if middle % 2 == 1:
         if check_middle(node, target):
-            p_node.next = node.next
+            if middle == 1:
+                head_node = None
+
+            else:
+                p_node.next = node.next
 
     else:
         if check_middle(p_node, target):
-            aux_node.next = node.next
+            if middle == 2:
+                head_node = head_node.next
+
+            else:
+                aux_node.next = p_node.next
 
         elif check_middle(node, target):
             p_node.next = node.next
 
-    return node
+    return head_node

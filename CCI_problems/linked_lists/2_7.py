3418fdbe3c1f147a035b3a390c0c9fb142d77fe0
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-13 02:45:42 -0500

    cleaning code

--- CCI_problems/linked_lists/2_7.py
+++ CCI_problems/linked_lists/2_7.py
@@ -5,17 +5,21 @@
 
 def get_intersection(node_1, node_2):
     if not node_1 or not node_2:
-        return 0
+        return None
 
     elements = {}
 
     while node_1:
         elements[node_1] = True
 
+        node_1 = node_1.next
+
 
     while node_2:
         if node_2 in elements:
             return node_2
 
+        node_2 = node_2.next
 
-    return "No intersection"
+
+    return None

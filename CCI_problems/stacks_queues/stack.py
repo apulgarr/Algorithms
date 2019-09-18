736c29de041d08d7c38e7346d5f86cdab01eb53c
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-18 18:58:49 -0500

    chapter 3 stacks and queues

---
+++ CCI_problems/stacks_queues/stack.py
@@ -0,0 +1,48 @@
+from node import Node
+
+class Node(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack(object):
+
+    def __init__(self):
+        self.top = None
+
+
+    def push(self, value):
+        node = Node(value)
+        node.next = self.top
+        self.top = node
+
+
+    def pop(self):
+        if self.is_empty():
+            return "empty stack"
+
+        data = self.top.value
+        self.top = self.top.next
+        return data
+
+
+    def is_empty(self):
+        return self.top == None
+
+
+    def peek(self):
+        if self.is_empty():
+            return "empty stack"
+
+        return self.top.value
+
+
+    def iterate_stack(self, node):
+        if node == None:
+            return 0
+
+        else:
+            print(node.value)
+            self.iterate_stack(node.next)

43f1877413d5baa43cb17f3ede926bc6755016f7
Andres Pulgarin <apulgar3@eafit.edu.co>
2019-09-18 19:37:17 -0500

    3_2

--- CCI_problems/stacks_queues/3_2.py
+++ CCI_problems/stacks_queues/3_2.py
@@ -5,17 +5,28 @@ class StackNew(object):
     def __init__(self, stack):
         self.stack = stack
         self.min_element = None
+        self.min_element_old = None
 
 
-    def set_min(self, element):
+    def set_min_push(self, element):
         if self.stack.is_empty():
+            self.min_element_old = self.min_element
             self.min_element = element
 
         else:
             if element < self.min_element:
-                min_element = element
+                self.min_element_old = self.min_element
+                self.min_element = element
 
         self.stack.push(element)
+
+
+
+    def set_min_pop(self):
+        value = self.stack.pop()
+
+        if value == self.min_element:
+            self.min_element = self.min_element_old
 
 
     def get_min(self):
